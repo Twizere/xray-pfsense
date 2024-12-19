6a7a93a439aff461212c0f66b3d1a49126802b27
Twizere <ptwizere@qtsoftwareltd.com>
2024-12-19 13:03:18 +0200

    try this

--- files/usr/local/www/xray/index.php
+++ files/usr/local/www/xray/index.php
@@ -25,7 +25,7 @@ echo "CA Certificates \n";
 cert_build_list('ca', 'Xray');
 $certs = cert_build_list('ca', 'Xray');
 foreach($certs as $cert){
-print_r(lookup_ca($cert['refid']));
+print_r(lookup_ca($cert[0]));
 }
 
 
